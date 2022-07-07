dde6d79c6d3743746d9f687ed19a69c57f922dc4
Steven Tomi <89354038+SteffQing@users.noreply.github.com>
2022-07-08 00:17:52 +0100

    Update

--- web.js
+++ web.js
@@ -1,66 +1,70 @@
-
-
-function scrollFunction() { 
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) { 
-    document.getElementById("logo").style.backgroundImage = "url('./assets/images/logo_img.png')"; 
-  } else { 
-    document.getElementById("logo").style.backgroundImage = "url('./assets/images/logo.png')"; 
-  } 
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("logo").style.backgroundImage =
+      "url('./assets/images/logo_img.png')";
+  } else {
+    document.getElementById("logo").style.backgroundImage =
+      "url('./assets/images/logo.png')";
+  }
 }
 
 //jumping to content at specific height
-const navheight = document.getElementById('navigation').offsetHeight;
-document.documentElement.style.setProperty('--scroll-padding', navheight - 1 + "px")
+const navheight = document.getElementById("navigation").offsetHeight;
+document.documentElement.style.setProperty(
+  "--scroll-padding",
+  navheight - 1 + "px"
+);
 
 //text animation
 let index = 0,
-    interval = 1000;
+  interval = 1000;
 
-const rand = (min, max) => 
-  Math.floor(Math.random() * (max - min + 1)) + min;
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-const animate = star => {
+const animate = (star) => {
   star.style.setProperty("--star-left", `${rand(-10, 100)}%`);
   star.style.setProperty("--star-top", `${rand(-40, 80)}%`);
 
   star.style.animation = "none";
   star.offsetHeight;
   star.style.animation = "";
-}
+};
 
 let timeouts = [],
-    intervals = [];
+  intervals = [];
 
 const magic = document.querySelector(".magic");
 
 magic.onmouseenter = () => {
   let index = 1;
-  
-  for(const star of document.getElementsByClassName("magic-star")) {
-    timeouts.push(setTimeout(() => {  
-      animate(star);
-      
-      intervals.push(setInterval(() => animate(star), 1000));
-    }, index++ * 300));
-  };
-}
+
+  for (const star of document.getElementsByClassName("magic-star")) {
+    timeouts.push(
+      setTimeout(() => {
+        animate(star);
+
+        intervals.push(setInterval(() => animate(star), 1000));
+      }, index++ * 300)
+    );
+  }
+};
 
 magic.onmouseleave = onMouseLeave = () => {
-  for(const t of timeouts) clearTimeout(t);  
-  for(const i of intervals) clearInterval(i);
-  
+  for (const t of timeouts) clearTimeout(t);
+  for (const i of intervals) clearInterval(i);
+
   timeouts = [];
   intervals = [];
-}
+};
 
 //roadmap animation
 function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
+  let reveals = document.querySelectorAll(".reveal");
 
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
 
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
@@ -72,16 +76,14 @@ function reveal() {
 
 window.addEventListener("scroll", reveal);
 
-
-
 //subheadings animation
 function intro() {
-  var intro = document.querySelectorAll(".intro");
+  let intro = document.querySelectorAll(".intro");
 
-  for (var i = 0; i < intro.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = intro[i].getBoundingClientRect().top;
-    var elementVisible = 80;
+  for (let i = 0; i < intro.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = intro[i].getBoundingClientRect().top;
+    let elementVisible = 80;
 
     if (elementTop < windowHeight - elementVisible) {
       intro[i].classList.add("active");
@@ -92,14 +94,13 @@ function intro() {
 window.addEventListener("scroll", intro);
 //product animation
 
-
 function marketplace() {
-  var marketplace = document.querySelectorAll(".marketplace");
+  let marketplace = document.querySelectorAll(".marketplace");
 
-  for (var i = 0; i < marketplace.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = marketplace[i].getBoundingClientRect().top;
-    var elementVisible = 50;
+  for (let i = 0; i < marketplace.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = marketplace[i].getBoundingClientRect().top;
+    let elementVisible = 50;
 
     if (elementTop < windowHeight - elementVisible) {
       marketplace[i].classList.add("active");
@@ -110,12 +111,12 @@ function marketplace() {
 window.addEventListener("scroll", marketplace);
 
 function lending() {
-  var lending = document.querySelectorAll(".lending");
+  let lending = document.querySelectorAll(".lending");
 
-  for (var i = 0; i < lending.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = lending[i].getBoundingClientRect().top;
-    var elementVisible = 50;
+  for (let i = 0; i < lending.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = lending[i].getBoundingClientRect().top;
+    let elementVisible = 50;
 
     if (elementTop < windowHeight - elementVisible) {
       lending[i].classList.add("active");
@@ -126,12 +127,12 @@ function lending() {
 window.addEventListener("scroll", lending);
 
 function game() {
-  var game = document.querySelectorAll(".game-guild");
+  let game = document.querySelectorAll(".game-guild");
 
-  for (var i = 0; i < game.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = game[i].getBoundingClientRect().top;
-    var elementVisible = 50;
+  for (let i = 0; i < game.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = game[i].getBoundingClientRect().top;
+    let elementVisible = 50;
 
     if (elementTop < windowHeight - elementVisible) {
       game[i].classList.add("active");
@@ -142,12 +143,12 @@ function game() {
 window.addEventListener("scroll", game);
 
 function bridge() {
-  var bridge = document.querySelectorAll(".bridge");
+  let bridge = document.querySelectorAll(".bridge");
 
-  for (var i = 0; i < bridge.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = bridge[i].getBoundingClientRect().top;
-    var elementVisible = 50;
+  for (let i = 0; i < bridge.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = bridge[i].getBoundingClientRect().top;
+    let elementVisible = 50;
 
     if (elementTop < windowHeight - elementVisible) {
       bridge[i].classList.add("active");
@@ -158,12 +159,12 @@ function bridge() {
 window.addEventListener("scroll", bridge);
 
 function launchpad() {
-  var launchpad = document.querySelectorAll(".launchpad");
+  let launchpad = document.querySelectorAll(".launchpad");
 
-  for (var i = 0; i < launchpad.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = launchpad[i].getBoundingClientRect().top;
-    var elementVisible = 50;
+  for (let i = 0; i < launchpad.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = launchpad[i].getBoundingClientRect().top;
+    let elementVisible = 50;
 
     if (elementTop < windowHeight - elementVisible) {
       launchpad[i].classList.add("active");
@@ -173,3 +174,16 @@ function launchpad() {
 
 window.addEventListener("scroll", launchpad);
 
+//bottom arrow: back to top
+myID = document.getElementById("myID");
+
+let myScrollFunc = function () {
+  let y = window.scrollY;
+  if (y >= 800) {
+    myID.className = "bottomMenu show";
+  } else {
+    myID.className = "bottomMenu hide";
+  }
+};
+
+window.addEventListener("scroll", myScrollFunc);
